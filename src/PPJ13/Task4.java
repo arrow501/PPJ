7865425060b205401284a65b0cbab48abcc6141b
Arrow <105371148+arrow501@users.noreply.github.com>
2022-11-23 15:56:24 +0100

    PPJ13 - Task4

--- src/PPJ13/Task4.java
+++ src/PPJ13/Task4.java
@@ -1,12 +1,14 @@
 package PPJ13;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Task4 {
     static Integer[] tab = {null,null, null};
     public static void main(String[] args) {
-        
-        read();
+        input = read();
+        number = Integer.parseInt(input.split(" ")[0]);
+        command = input.split(" ")[1];
     }
 
     private static void print() {
@@ -17,14 +19,36 @@ public class Task4 {
             System.out.println();
         }
     }
+
     private static String read(){
         java.util.Scanner in = new java.util.Scanner(System.in);
-        String input = in.nextLine();
-        
-        System.out.println(input.split(" ")[1]);
-        return input;
+        try {
+            System.out.println("Input a number followed by a command. Type help for list of commands.");
+            String input = in.nextLine();
+            in.close();
+            if(input == "help"){
+                help();
+                return read();
+            }
+            return input;
+        } catch (Exception e) {
+            System.out.println("Incorrect input.\r\n" + "Example input: 5 add");
+            return read();
+        }
     }
+    private static void exit() {
+        print();
+        System.exit(0);
+    }
+
+    private static void help() {
+        // TODO: add help
+    }
+
     private static void add(){
+
+    }
+    private static void get(){
 
     }
 
